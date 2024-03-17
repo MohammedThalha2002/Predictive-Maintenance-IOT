e3d5ba58853acd5737489f9f45488f079ddf90d9
MohammedThalha2002 <mohammedalithalha2002@gmail.com>
2024-03-17 22:20:26 +0530

    updated

--- BACKEND/index.js
+++ BACKEND/index.js
@@ -7,14 +7,11 @@ const app = express();
 
 const PORT = 3000;
 
-app.use(cors());
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-  res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
-  next();
-});
+app.use(cors({
+  origin: '*',
+  credentials: true
+}));
+
 app.use(express.json());
 
 let sensorData = {
